2f6c433d8aff6c01d77da89d676cc90d03f8c13b
Ross Crichton <rcrichton87@googlemail.com>
2017-04-18 15:32:12 +0100

    shipQuery file written

---
+++ controllers/ships.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var shipRouter = express.Router();
+var ShipQuery = require('../db/shipQuery');
+var shipQuery = new FilmQuery();
---
+++ db/shipQuery.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var ShipQuery = function(){
+  this.url = "mongodb://localhost:27017/mongo_homework";  //only the last bit changes to the database name
+};
+
+ShipQuery.prototype = {
+  all: function(onQueryFinished){ 
+    MongoClient.connect(this.url, function(err, db){ 
+      if(db){
+        var collection = db.collection('starwars');
+        collection.find().toArray(function(err, docs){ 
+          onQueryFinished(docs); 
+        });       
+      }
+    });
+  },
