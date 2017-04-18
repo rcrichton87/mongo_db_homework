157772a788bf9ebdc21e430fad2d42b425930653
Ross Crichton <rcrichton87@googlemail.com>
2017-04-18 15:26:26 +0100

    seeds file filled in

---
+++ seeds.js
@@ -0,0 +1,21 @@
+use mongo_homework;
+
+db.starwars.insert([
+  {
+    name: "X-Wing",
+    speed: 100
+  },
+  {
+    name: "Y-Wing",
+    speed: 80
+  },
+  {
+    name: "A-Wing",
+    speed: 120
+  },
+  {
+    name: "B-Wing",
+    speed: 91
+  }
+
+]);
